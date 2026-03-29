c9994ec0274971b11b41ed411be678b7b3da4cf6
Thietam <tranthientam220404@gmail.com>
2026-03-29 15:33:21 +0700

    feat: update profile user

--- backend/user_service/src/routes/user.routes.js
+++ backend/user_service/src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getProfile } = require("../controllers/user.controller");
+const { getProfile, updateUser } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.get("/profile", getProfile);
+router.put("/profile", updateUser);
 
 module.exports = router;
