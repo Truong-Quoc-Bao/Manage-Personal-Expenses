e025b0637661ff4f1b08ba4f85cd08ab826bbd64
Thietam <tranthientam220404@gmail.com>
2026-03-30 01:00:37 +0700

    feat: delete account

--- backend/account_service/src/routes/account.routes.js
+++ backend/account_service/src/routes/account.routes.js
@@ -3,10 +3,12 @@ const {
   CreateAccount,
   getAccountsController,
   updateAccountController,
+  deleteAccountController,
 } = require("../controllers/account.controller");
 
 const router = express.Router();
 router.post("/accounts", CreateAccount);
 router.get("/accounts", getAccountsController);
 router.put("/accounts", updateAccountController);
+router.delete("/accounts", deleteAccountController);
 module.exports = router;
