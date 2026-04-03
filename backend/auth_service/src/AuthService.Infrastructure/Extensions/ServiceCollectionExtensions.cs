d34215e447e9c3157921c736d63961f562b86c72
letandat1607 <167631642+letandat1607@users.noreply.github.com>
2026-04-04 01:09:01 +0700

    feat: add password strength validation

--- backend/auth_service/src/AuthService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ backend/auth_service/src/AuthService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -41,10 +41,10 @@ namespace AuthService.Infrastructure.Extensions
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredLength = 6;
+                options.Password.RequiredLength = 8;
                 options.Password.RequiredUniqueChars = 1;
 
                 options.User.RequireUniqueEmail = true;
