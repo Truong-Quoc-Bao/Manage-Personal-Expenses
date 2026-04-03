8bb0fda5d652f8d342315c9a9250c390fd6c4880
letandat1607 <dat16072004@gmail.com>
2026-04-03 16:42:22 +0700

    feat: create auth table schema

--- backend/auth_service/src/AuthService.Core/DTOs/UserRegistrationEvent.cs
+++ backend/auth_service/src/AuthService.Core/DTOs/UserRegistrationEvent.cs
@@ -2,10 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AuthService.Core.DTOs
 {
-    public class UserRegistrationFailedEvent
+    public class UserRegistrationEvent
     {
         [Required]
         public string UserId { get; set; }
         public string Reason { get; set; }
+        public string Status { get; set; }
     }
 }
