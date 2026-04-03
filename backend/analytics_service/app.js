9fd6208c1b7797545fcf94ad31319f4d24c5403f
Tran-Gia-Phuc <giaphuc031204@gmail.com>
2026-04-03 14:54:58 +0700

    feat: connect-analytics-service-successfull

--- backend/analytics_service/app.js
+++ backend/analytics_service/app.js
@@ -3,6 +3,8 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+
+const connectDB = require('./src/config/database');
 const rabbitMQClient = require('./src/events');
 
 app.use(express.json());
@@ -12,7 +14,25 @@ app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
 
-app.listen(PORT, async () => {
-    await rabbitMQClient.startRabbitMQ();
-    console.log(`Analytics service is running on port ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB(); // 👈 connect Mongo trước
+        await rabbitMQClient.startRabbitMQ();
+
+        app.listen(PORT, () => {
+            console.log(`Analytics service is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
+};
+
+startServer();
+
+// app.listen(PORT, async () => {
+//     await connectDB();
+//     await rabbitMQClient.startRabbitMQ();
+//     console.log(`Analytics service is running on port ${PORT}`);
+// });
+
+
