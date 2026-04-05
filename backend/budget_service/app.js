69f68bc3cb18ecc0b8e03ca8b0927d75ca5d36ca
letandat1607 <dat16072004@gmail.com>
2026-04-05 16:43:59 +0700

    feat: add budget service

---
+++ backend/budget_service/app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+// const rabbitMQClient = require('./src/events');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+app.listen(PORT, async () => {
+    // rabbitMQClient.startRabbitMQ();
+    console.log(`Budget service is running on port ${PORT}`);
+});
