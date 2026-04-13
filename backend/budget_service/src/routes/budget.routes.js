b651861cb1dd87795340a65c148a2355a90445e3
Thietam <tranthientam220404@gmail.com>
2026-04-13 14:48:25 +0700

    feat: update budget by budgetId

--- backend/budget_service/src/routes/budget.routes.js
+++ backend/budget_service/src/routes/budget.routes.js
@@ -3,6 +3,7 @@ const {
   getBudgetByUserIdController,
   getBudgetByBudgetIdController,
   createBudgetController,
+  updateBudgetController,
 } = require("../controllers/budget.controller");
 const validate = require("../middlewares/validation.middleware");
 
@@ -11,4 +12,5 @@ const router = express.Router();
 router.get("/budgets", validate(), getBudgetByUserIdController);
 router.get("/budgets/:id", getBudgetByBudgetIdController);
 router.post("/budgets", validate(), createBudgetController);
+router.put("/budgets/:id", validate(), updateBudgetController);
 module.exports = router;
