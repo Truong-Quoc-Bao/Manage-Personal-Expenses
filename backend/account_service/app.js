7bd2b84307cc44c31c489e134ffe00fbd2541881
Trương Quốc Bảo <166063783+Truong-Quoc-Bao@users.noreply.github.com>
2026-06-05 02:38:34 +0700

    Final fix for Render deployment: fix app.js analytic  and updated blueprint

--- backend/account_service/app.js
+++ backend/account_service/app.js
@@ -1,33 +1,45 @@
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
-const express = require("express");
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
-const rabbitMQClient = require("./src/events");
-const {startGrpcServer} = require("./src/grpc/account.grpc");
+const rabbitMQClient = require('./src/events');
+const { startGrpcServer } = require('./src/grpc/account.grpc');
 
-const accountRouter = require("./src/routes/account.routes");
+const accountRouter = require('./src/routes/account.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
 });
 
-app.use("/", accountRouter);
+app.use('/', accountRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
 
   return res.status(err.statusCode || 500).json({
     success: false,
-    message: err.message || "Internal server error",
+    message: err.message || 'Internal server error',
   });
 });
 
+// app.listen(PORT, async () => {
+//   await rabbitMQClient.startRabbitMQ();
+//   console.log(`Account service is running on port ${PORT}`);
+//   startGrpcServer();
+// });
+
 app.listen(PORT, async () => {
+  console.log('STEP 1');
+
   await rabbitMQClient.startRabbitMQ();
+
+  console.log('STEP 2');
   console.log(`Account service is running on port ${PORT}`);
   startGrpcServer();
+
+  console.log('STEP 3');
 });
