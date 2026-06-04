ede3caba1df578cc18ca28ae00d9cb3dc2853759
Trương Quốc Bảo <166063783+Truong-Quoc-Bao@users.noreply.github.com>
2026-06-04 18:02:45 +0700

    Configure blueprint and env groups

--- backend/analytics_service/app.js
+++ backend/analytics_service/app.js
@@ -3,16 +3,14 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3004;
 
 const connectDB = require('./src/config/database');
 const rabbitMQClient = require('./src/events');
 const router = require('./src/routes/analytics.routes');
 // const {startGrpcServer} = require("./src/grpc/analytics.grpc");
 
-
-
-const API_PREFIX = process.env.API_PREFIX || "";
+const API_PREFIX = process.env.API_PREFIX || '';
 
 // ── Middlewares ───────────────────────────────
 app.use(express.json());
@@ -23,34 +21,33 @@ app.use(API_PREFIX, router);
 // const router = require('./src/routes/analytics.routes');
 
 // health check
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
 });
-
 
 // ── Test API (optional) ───────────────────────
 app.get('/test', (req, res) => {
-    res.json({
-        message: 'Analytics service is working 🚀'
-    });
+  res.json({
+    message: 'Analytics service is working 🚀',
+  });
 });
 
 // ── Main routes ───────────────────────────────
-app.use("/", router);
+app.use('/', router);
 // routes
 
 const startServer = async () => {
-    try {
-        await connectDB(); // connect Mongo trước
-        await rabbitMQClient.startRabbitMQ();
+  try {
+    await connectDB(); // connect Mongo trước
+    await rabbitMQClient.startRabbitMQ();
 
-        app.listen(PORT, () => {
-            console.log(`Analytics service is running on port ${PORT}`);
-            // startGrpcServer();
-        });
-    } catch (error) {
-        console.error('Error starting server:', error);
-    }
+    app.listen(PORT, () => {
+      console.log(`Analytics service is running on port ${PORT}`);
+      // startGrpcServer();
+    });
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
 };
 
 startServer();
@@ -60,4 +57,3 @@ startServer();
 //     await rabbitMQClient.startRabbitMQ();
 //     console.log(`Analytics service is running on port ${PORT}`);
 // });
-
